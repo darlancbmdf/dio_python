5b71f0846edd70e70b4cb753b8573a0ffe82f3be
darlancbmdf <darlan.cbmdf@gmail.com>
2022-05-26 17:39:17 -0300

    aula 7, calculadora e televisão

---
+++ Introducao ao Python/aula7_calculadora2.py
@@ -0,0 +1,21 @@
+class Calculadora:
+  def __init__(self):
+    pass
+  
+  def soma(self, valor_a, valor_b):
+    return valor_a + valor_b
+
+  def subtracao(self, valor_a, valor_b):
+    return valor_a - valor_b
+
+  def multiplicacao(self, valor_a, valor_b):
+    return valor_a * valor_b
+
+  def divisao(self, valor_a, valor_b):
+    return valor_a / valor_b
+
+calculadora = Calculadora()
+print(calculadora.soma(10, 2))
+print(calculadora.subtracao(10, 2))
+print(calculadora.multiplicacao(10, 2))
+print(calculadora.divisao(10, 2))
